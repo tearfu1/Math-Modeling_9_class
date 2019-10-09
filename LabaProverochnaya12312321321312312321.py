72cf91680c6b1ea3de8ec77840143827c8b0bd0c
tearfu1 <56321070+tearfu1@users.noreply.github.com>
2019-10-09 15:58:11 +0200

    YA MOLODEC SDELAL LABY №1

---
+++ LabaProverochnaya12312321321312312321.py
@@ -0,0 +1,35 @@
+print("-------Задание №1-----")
+print("Hellow, World!")
+print("Маша + Петя = Любовь")
+print("x = 3 + 4")
+x = 3 + 4
+print("x =",x)
+print()
+
+print("-------Задание №2-----")
+x = "Hello World"
+print(type(x))
+x = 3 + 4
+print(type(x))
+x = 3 / 4
+print(type(x))
+x = [1,2,5,10,100]
+print(type(x))
+print()
+
+print("-------Задание №3-----")
+x = 3
+y = (x**0.5**2/(x**3+3/x))*(4*x**7-x**5)+80*((27*x**4+12*x**3-5*x**2+10)**0.5)
+print(y)
+y = (3%2+(16.7*4.32)//1)/(14.5+(31%12)-(x**3.4//1))
+print(y)
+print()
+
+print("-------Задание №4-----")
+a = [1,5,'Good', 'Bad']
+b = [9,'Blue', 'Red', 11]
+print(a[1]+b[3])
+print(a[2]+b[2])
+print(a[0]*b[0])
+print(a[1]**b[3])
+print(a+b)
