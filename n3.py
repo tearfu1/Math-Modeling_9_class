6fdd77b417dc0cd5c27d8494ee65e59f3ceeecd2
tearfu1 <56321070+tearfu1@users.noreply.github.com>
2019-11-13 16:43:40 +0200

    Delete n3.py

--- n3.py
+++
@@ -1,8 +0,0 @@
-from const import svobodnoe_padenie as g
-
-def energy(mass, height, speed):
-    potential=mass*height*g
-    kinetic=(mass*speed**2)/2
-    en=kinetic+potential
-    print(en)
-energy(1, 1, 1)
